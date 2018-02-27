bc611fd1fc28a0d35f7c8d3ad9c4eba523f2e0fd
seungjuhan <wade3han@wafflestudio.com>
2018-02-27 11:26:27 +0900

    Fixed errors: pallet

--- backend/showlist/urls.py
+++ backend/showlist/urls.py
@@ -6,8 +6,8 @@ from . import views
 urlpatterns = [
     path('company/<int:pk>', views.CompanyRetrieveUpdateDestroy.as_view()),
     path('company/', views.CompanyListCreate.as_view()),
-    path('pallet/<int:pk>', views.PalletListCreate.as_view()),
-    path('pallet/', views.PalletRetrieveUpdateDestroy.as_view()),
+    path('pallet/<int:pk>', views.PalletRetrieveUpdateDestroy.as_view()),
+    path('pallet/', views.PalletListCreate.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
